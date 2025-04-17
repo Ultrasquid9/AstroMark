f79d5f76cdb5a693b265da7a54246bf08f77bae8
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-04-17 16:45:02 -0400

    Config Reset Flag

--- src/init.rs
+++ src/init.rs
@@ -4,8 +4,8 @@ use std::{
 	path::{Path, PathBuf},
 };
 
-use clap::{ArgMatches, Command, value_parser};
-use tracing::{Level, error};
+use clap::{ArgAction, ArgMatches, Command, value_parser};
+use tracing::{Level, error, info};
 
 use crate::{AppResult, app::flags::Flags};
 
@@ -15,16 +15,24 @@ pub fn args() -> ArgMatches {
 		.about("Super simple markdown editor")
 		.arg(
 			clap::arg!(-c --config <FILE> "Use a custom config file")
-				.required(false)
+				//.required(false)
 				.value_parser(value_parser!(PathBuf)),
 		)
+		.arg(clap::arg!(-r --reset_config ... "Reset the config file").action(ArgAction::SetTrue))
 		.get_matches()
 }
 
 pub fn flags(args: &ArgMatches) -> Flags {
+	if args.get_flag("reset_config") {
+		info!("Resetting config!");
+		if let Err(e) = fs::write(get_or_create_cfg_dir(), Flags::default_str()) {
+			error!("{e}")
+		}
+	}
+
 	let dir = match args.get_one::<PathBuf>("config") {
 		Some(dir) => dir,
-		None => &default_cfg_dir(),
+		None => &get_or_create_cfg_dir(),
 	};
 
 	Flags::read(dir)
@@ -39,41 +47,35 @@ pub fn log() -> AppResult<()> {
 	Ok(())
 }
 
-fn default_cfg_dir() -> PathBuf {
-	let mut dir = cfg_dir();
-	dir.push("config.ron");
+fn get_or_create_cfg_dir() -> PathBuf {
+	let Some(mut dir) = dirs::config_dir() else {
+		error!("Config dir could not be found");
+		panic!()
+	};
 
-	dir_exists_or_run(&dir, || fs::write(&dir, Flags::default_str()));
+	dir.push("estro_md");
+	dir_exists_or_run(&dir, fs::create_dir_all);
+
+	dir.push("config.ron");
+	dir_exists_or_run(&dir, |pat| {
+		info!("No config file detected, creating one now...");
+		fs::write(pat, Flags::default_str())
+	});
 
 	dir
-}
-
-fn cfg_dir() -> PathBuf {
-	match dirs::config_dir() {
-		Some(mut dir) => {
-			dir.push("estro_md");
-			dir_exists_or_run(&dir, || fs::create_dir_all(&dir));
-
-			dir
-		}
-		None => {
-			error!("Config dir could not be found");
-			panic!()
-		}
-	}
 }
 
 /// Checks if a directory exists. If it does not, runs the provided function and handles any errors.
 fn dir_exists_or_run<Pat, Fun, Err, Idk>(dir: Pat, fun: Fun)
 where
 	Pat: AsRef<Path>,
-	Fun: Fn() -> Result<Idk, Err>,
+	Fun: Fn(Pat) -> Result<Idk, Err>,
 	Err: Error,
 {
 	// TODO: Replace with let-chain once stabilized
 	if let Ok(exists) = fs::exists(&dir) {
 		if !exists {
-			if let Err(e) = fun() {
+			if let Err(e) = fun(dir) {
 				error!("{e}")
 			}
 		}
